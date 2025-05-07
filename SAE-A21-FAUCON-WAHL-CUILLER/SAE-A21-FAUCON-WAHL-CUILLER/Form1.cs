4b973d12ed29c26d9d134321d02c135388656732
Mathéo Faucon <fcnmatheo@gmail.com>
2025-05-07 08:51:02 +0200

    Add original project

---
+++ SAE-A21-FAUCON-WAHL-CUILLER/SAE-A21-FAUCON-WAHL-CUILLER/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SAE_A21_FAUCON_WAHL_CUILLER
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
